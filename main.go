da8c2abfe58cc1f4e9b10fac60690ea655d8e7db
Cor Zuurmond <jczuurmond@protonmail.com>
2021-03-26 11:54:07 +0100

    Fix indent

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-"terraform-provider-github/github"
+	"terraform-provider-github/github"
 )
 
 func main() {
