20c3a3632d8a387c2f351c7093b36ac10abe7f83
Cor Zuurmond <jczuurmond@protonmail.com>
2021-03-26 11:02:36 +0100

    Add provider configure

--- github/provider.go
+++ github/provider.go
@@ -3,6 +3,9 @@ package github
 import (
 	"context"
 
+	"golang.org/x/oauth2"
+
+	"github.com/google/go-github/v34/github"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -21,4 +24,32 @@ func Provider() *schema.Provider {
 		ResourcesMap:   map[string]*schema.Resource{},
 		DataSourcesMap: map[string]*schema.Resource{},
 	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	token := d.Get("token").(string)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	if (token != "") {
+		ctx := context.Background()
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: token},
+		)
+		tc := oauth2.NewClient(ctx, ts)
+
+		client, err := github.NewClient(tc)
+
+		if err != nil {
+			return nil, diag.FromErr(err)
+		}
+
+		// list all repositories for the authenticated user
+		repos, _, err := client.Repositories.List(ctx, "", nil)
+
+		return client, diags
+	}
+
+	return nil, diags
 }
