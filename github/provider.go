76f21859366d75188db02ecafae2e3bbf51049f4
Cor Zuurmond <jczuurmond@protonmail.com>
2021-03-26 10:55:44 +0100

    Add provider context

--- github/provider.go
+++ github/provider.go
@@ -1,12 +1,23 @@
 package github
 
 import (
-"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 // Provider -
 func Provider() *schema.Provider {
 	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"token": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    false,
+				Sensitive:   true,
+				DefaultFunc: schema.EnvDefaultFunc("GITHUB_TOKEN", nil),
+			},
+		},
 		ResourcesMap:   map[string]*schema.Resource{},
 		DataSourcesMap: map[string]*schema.Resource{},
 	}
